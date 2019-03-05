86f6148de85d4fd1f8f6fffc58b6ee9b3b290c2e
李泰儀 <leetaiyi@litaiyide-macbook-pro.local>
2019-03-05 15:28:16 +0800

    Convert to MVC design pattern

---
+++ ToDoList/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  ToDoList
+//
+//  Created by 李泰儀 on 2019/3/5.
+//  Copyright © 2019 TerryLee. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title: String = ""
+    var done: Bool = false
+}
